36bbc490c71b80a88a030d501e131b4c1f37ed36
NartovEvgenii <evgeniynartov36rus@gmail.com>
2020-11-08 02:31:39 +0300

    add findBY test

--- NetCrecProject/src/main/java/SortedPackage/ISorted.java
+++ NetCrecProject/src/main/java/SortedPackage/ISorted.java
@@ -7,12 +7,19 @@ public interface ISorted {
 
     void sort(Contract[] masContracts, Comparator<Contract> comp);
 
-    public static int checkCompWithNull(Contract o1, Contract o2, Comparator<Contract> comp) {        
-        if (o1 != null && o2 != null) {
-                return comp.compare(o1, o2);
+    /**
+     * Method returns the values of the standard comparator, taking NULL as the maximum value.
+     * @param contr1 - first contract
+     * @param contr2 - second contract
+     * @param comp - comparator sort criterion
+     * @return - 0 if both contracts are null, 1 if first contract is not null,-1 if second contract is not null
+     */
+    public static int checkCompWithNull(Contract contr1, Contract contr2, Comparator<Contract> comp) {        
+        if (contr1 != null && contr2 != null) {
+                return comp.compare(contr1, contr2);
             } else {
-                if (o1 != null || o2 != null) {
-                    return o1 != null ? -1 : 1;
+                if (contr1 != null || contr2 != null) {
+                    return contr1 != null ? -1 : 1;
                 } else {
                     return 0;
                 }
--- NetCrecProject/src/test/java/ContractPackage/ContractStoreTest.java
+++ NetCrecProject/src/test/java/ContractPackage/ContractStoreTest.java
@@ -1,21 +1,23 @@
 package ContractPackage;
 
 import java.time.LocalDate;
+import java.util.function.Predicate;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ContractStoreTest {
-    
+
     public ContractStoreTest() {
     }
+
     /**
      * Test of addContract method, of class ContractStore.
      */
     @Test
     public void testAddContract() {
         System.out.println("addContract");
-        Client cli = new Client(1,"Ivan","Ivanov","Ivanovich",LocalDate.of(1984, 7, 28),Gender.Male,1,1);
-        Contract contr = new CellularContract(1,LocalDate.of(2010, 7, 28),LocalDate.of(2040, 7, 28),1,cli,1,1,1);
+        Client cli = new Client(1, "Ivan", "Ivanov", "Ivanovich", LocalDate.of(1984, 7, 28), Gender.Male, 1, 1);
+        Contract contr = new CellularContract(1, LocalDate.of(2010, 7, 28), LocalDate.of(2040, 7, 28), 1, cli, 1, 1, 1);
         ContractStore instance = new ContractStore();
         instance.addContract(contr);
         // TODO review the generated test code and remove the default call to fail.
@@ -29,9 +31,9 @@ public class ContractStoreTest {
     public void testGetContract() {
         System.out.println("getContract");
         int id = 0;
-        ContractStore instance = new ContractStore();        
-        Client cli = new Client(1,"Ivan","Ivanov","Ivanovich",LocalDate.of(1984, 7, 28),Gender.Male,1,1);
-        Contract contr = new CellularContract(1,LocalDate.of(2010, 7, 28),LocalDate.of(2040, 7, 28),1,cli,1,1,1);
+        ContractStore instance = new ContractStore();
+        Client cli = new Client(1, "Ivan", "Ivanov", "Ivanovich", LocalDate.of(1984, 7, 28), Gender.Male, 1, 1);
+        Contract contr = new CellularContract(1, LocalDate.of(2010, 7, 28), LocalDate.of(2040, 7, 28), 1, cli, 1, 1, 1);
         instance.addContract(contr);
         Contract result = instance.getContract(id);
         assertEquals(contr, result);
@@ -46,9 +48,9 @@ public class ContractStoreTest {
     public void testGetContractByID() {
         System.out.println("getContractByID");
         int idcontr = 1;
-        ContractStore instance = new ContractStore();        
-        Client cli = new Client(1,"Ivan","Ivanov","Ivanovich",LocalDate.of(1984, 7, 28),Gender.Male,1,1);
-        Contract contr = new CellularContract(1,LocalDate.of(2010, 7, 28),LocalDate.of(2040, 7, 28),1,cli,1,1,1);
+        ContractStore instance = new ContractStore();
+        Client cli = new Client(1, "Ivan", "Ivanov", "Ivanovich", LocalDate.of(1984, 7, 28), Gender.Male, 1, 1);
+        Contract contr = new CellularContract(1, LocalDate.of(2010, 7, 28), LocalDate.of(2040, 7, 28), 1, cli, 1, 1, 1);
         instance.addContract(contr);
         Contract result = instance.getContractByID(idcontr);
         assertEquals(contr, result);
@@ -90,13 +92,39 @@ public class ContractStoreTest {
         System.out.println("getColCoctr");
         ContractStore instance = new ContractStore();
         int expResult = 1;
-        Client cli = new Client(1,"Ivan","Ivanov","Ivanovich",LocalDate.of(1984, 7, 28),Gender.Male,1,1);
-        Contract contr = new CellularContract(1,LocalDate.of(2010, 7, 28),LocalDate.of(2040, 7, 28),1,cli,1,1,1);
+        Client cli = new Client(1, "Ivan", "Ivanov", "Ivanovich", LocalDate.of(1984, 7, 28), Gender.Male, 1, 1);
+        Contract contr = new CellularContract(1, LocalDate.of(2010, 7, 28), LocalDate.of(2040, 7, 28), 1, cli, 1, 1, 1);
         instance.addContract(contr);
         int result = instance.getColCoctr();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
-    
+
+    /**
+     * Test of findBy method, of class ContractStore.
+     */
+    @Test
+    public void testfindBy() {
+        System.out.println("getColCoctr");
+        ContractStore instance = new ContractStore();
+        Client cli = new Client(1, "Ivan", "Ivanov", "Ivanovich", LocalDate.of(1984, 7, 28), Gender.Male, 1, 1);
+        int[] masIndex = new int[]{10, 11, 3, 22, 4};
+        for (int i = 0; i < 5; i++) {
+            Contract contr = new Contract(masIndex[i], LocalDate.of(2010, 7, 28), LocalDate.of(2040, 7, 28), 1, cli);
+            instance.addContract(contr);
+        }
+        Predicate<Contract> checkID = contr -> contr.getId() >= 10;
+        Contract[] mas = instance.findBy(checkID);
+        for (int i = 0; i < 5; i++) {
+            if (mas[i] != null) {
+                assert (mas[i].getId() >= 10);
+            } else {
+                break;
+            }
+        }
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
 }
